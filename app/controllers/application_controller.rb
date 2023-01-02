8f41329db5a1c927ffae071b7e48839036ab870d
Bailey Cage <bailey.cage@gmail.com>
2023-01-02 14:04:37 -0500

    setup rails

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::API
+  include ActionController::Cookies
+
+  # testing cookies
+  def hello_world
+    session[:count] = (session[:count] || 0) + 1
+    render json: { count: session[:count] }
+  end
+
+end
